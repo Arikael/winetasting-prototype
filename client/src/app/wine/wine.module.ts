20e579c48caa7a822773ef5cbbe97467278570c7
Arikael <arikael@mailbox.org>
2020-04-11 16:10:03 +0200

    added winewizard

--- client/src/app/wine/wine.module.ts
+++ client/src/app/wine/wine.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AddWineComponent } from './add-wine/add-wine.component';
+import { AddWineComponent } from './edit-wine/add-wine.component';
 import { WineRoutingModule } from './wine-routing.module';
 import { IonicModule } from '@ionic/angular';
 import { BaseDataComponent } from './base-data/base-data.component';

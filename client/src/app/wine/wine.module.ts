01a6615d4535ab0b2807daa65d1462f2fe64ce5d
Arikael <arikael@mailbox.org>
2020-04-11 11:14:36 +0200

    add header

--- client/src/app/wine/wine.module.ts
+++ client/src/app/wine/wine.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AddWineComponent } from './add-wine/add-wine.component';
 import { WineRoutingModule } from './wine-routing.module';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [
     AddWineComponent
   ],
   imports: [
+    IonicModule,
     CommonModule,
     WineRoutingModule
   ]

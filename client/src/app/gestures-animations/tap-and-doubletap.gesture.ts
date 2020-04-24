1d1773a1d0f785b2b79890b7727c85bed143ca17
Arikael <arikael@mailbox.org>
2020-04-24 14:58:45 +0200

    added tap/double tap gesture

---
+++ client/src/app/gestures-animations/tap-and-doubletap.gesture.ts
@@ -0,0 +1,21 @@
+export function createTapAndDoubleTapGestureOnStart(onTap: () => void, onDoubleTap: () => void, doubleTapThreshold: number) {
+    return (() => {
+        let timeout = null;
+        let lastOnStart = 0;
+
+        return () => {
+            const now = Date.now();
+            console.log('test');
+
+            if (Math.abs(now - lastOnStart) <= doubleTapThreshold) {
+                clearTimeout(timeout);
+                console.log('double');
+                lastOnStart = 0;
+                onDoubleTap();
+            } else {
+                lastOnStart = now;
+                timeout = setTimeout(() => onTap(), doubleTapThreshold);
+            }
+        };
+    })();
+}

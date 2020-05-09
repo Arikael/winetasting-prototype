b4bd0242309b3a4d9ee56ea0bfaf0a880627ddc2
Arikael <arikael@mailbox.org>
2020-05-09 12:16:31 +0200

    move ion-content

--- client/src/app/wine/add-wine/nose/nose.component.ts
+++ client/src/app/wine/add-wine/nose/nose.component.ts
@@ -1,14 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TasteCategory } from '../../models/taste-category';
+import { TasteService } from '../../services/taste.service';
 
 @Component({
   selector: 'app-nose',
   templateUrl: './nose.component.html',
   styleUrls: ['./nose.component.scss'],
 })
-export class NoseComponent implements OnInit {
+export class NoseComponent implements OnInit, OnChanges {
+  keys: Observable<any[]>;
+  get tasteCategories(): Observable<TasteCategory[]> {
+    return this.tasteService.getTasteCatgories('');
+  }
 
-  constructor() { }
+  constructor(private tasteService: TasteService) { }
+  ngOnChanges(changes: import('@angular/core').SimpleChanges): void {
+    console.log(changes);
+  }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.keys = this.tasteService.getTasteCatgories('');
+  }
 
 }
